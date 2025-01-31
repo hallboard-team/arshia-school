0e9206b11d6caf1529e484a9b197be4487bf3326
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-01-31 23:22:29 +0330

    not-found page Design done

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
