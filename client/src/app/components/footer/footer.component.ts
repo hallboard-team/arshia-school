28f9c06b40af1434e27f754c40a8e2b0b2618101
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-05-29 19:52:31 +0330

    design footer in mobile and tablet done

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon, MatDividerModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
