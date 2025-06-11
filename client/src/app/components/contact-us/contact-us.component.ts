3f7824444e3e8c63fbea9106cb26e0f1fc8180dd
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-06-11 18:53:31 +0330

    Redesign contact-us done

--- client/src/app/components/contact-us/contact-us.component.ts
+++ client/src/app/components/contact-us/contact-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent, MatIcon
   ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
