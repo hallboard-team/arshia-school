50392c52b6162b227fe83e9a7ad673f9e62a0b2c
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-06-12 15:58:52 +0330

    design navigation back and forward buttons and debug margin in whole application

--- client/src/app/components/back-forward-button/back-forward-button.component.ts
+++ client/src/app/components/back-forward-button/back-forward-button.component.ts
@@ -2,11 +2,14 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { HistoryService } from '../../services/history.service';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-back-forward-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, MatIcon
+  ],
   templateUrl: './back-forward-button.component.html',
   styleUrl: './back-forward-button.component.scss'
 })
@@ -14,13 +17,21 @@ export class BackForwardButtonComponent {
   history = inject(HistoryService);
   router = inject(Router);
 
+  lastClicked: 'back' | 'forward' | null = null;
+
   goBack() {
     const prevUrl = this.history.back();
-    if (prevUrl) this.router.navigateByUrl(prevUrl);
+    if (prevUrl) {
+      this.lastClicked = 'back';
+      this.router.navigateByUrl(prevUrl);
+    }
   }
 
   goForward() {
     const nextUrl = this.history.forward();
-    if (nextUrl) this.router.navigateByUrl(nextUrl);
+    if (nextUrl) {
+      this.lastClicked = 'forward';
+      this.router.navigateByUrl(nextUrl);
+    }
   }
 }
