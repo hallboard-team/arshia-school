0b3dd9b41ece09f56e53210299a1fe8f3aeb05c5
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-06-30 16:21:59 +0330

    add start day in course-card

--- client/src/app/components/courses/course-card/course-card.component.ts
+++ client/src/app/components/courses/course-card/course-card.component.ts
@@ -6,6 +6,8 @@ import { RouterModule } from '@angular/router';
 import { Course, ShowCourse } from '../../../models/course.model';
 import { LoggedInUser } from '../../../models/logged-in-user.model';
 import { AccountService } from '../../../services/account.service';
+import moment from 'moment-jalaali';
+moment.loadPersian({ dialect: 'persian-modern', usePersianDigits: false });
 
 @Component({
   selector: 'app-course-card',
@@ -17,14 +19,29 @@ import { AccountService } from '../../../services/account.service';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss'
 })
-export class CourseCardComponent implements OnInit{
+export class CourseCardComponent implements OnInit {
+  // @Input('courseInput') courseIn: ShowCourse | undefined;
+
+  // private _accountService = inject(AccountService);
+
+  // loggedInUserSig: Signal<LoggedInUser | null> | undefined;
+
+  // ngOnInit(): void {
+  //   this.loggedInUserSig = this._accountService.loggedInUserSig;
+  // }
   @Input('courseInput') courseIn: ShowCourse | undefined;
 
+  shamsiStartDate: string = ''; // اینو اضافه کن
+
   private _accountService = inject(AccountService);
-  
   loggedInUserSig: Signal<LoggedInUser | null> | undefined;
-  
+
   ngOnInit(): void {
     this.loggedInUserSig = this._accountService.loggedInUserSig;
+
+    if (this.courseIn?.start) {
+      // تبدیل تاریخ میلادی به شمسی
+      this.shamsiStartDate = moment(this.courseIn.start).format('jYYYY/jMM/jDD');
+    }
   }
 }
