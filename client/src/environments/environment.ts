792c333cdc9c3d81dfeb50ca0c3efc950b58d0f6
Reza Taba <mrtabaa@gmail.com>
2025-08-07 12:22:29 +0400

    Added production urls

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiUrl: "//wa-arshia-api-school-dsbbcnh2dxg3hyaj.eastus-01.azurewebsites.net/api/",
+  apiPhotoUrl: "//wa-arshia-api-school-dsbbcnh2dxg3hyaj.eastus-01.azurewebsites.net/"
+};
