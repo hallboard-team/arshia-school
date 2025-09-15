90922401bbadcf36cb6b6c6c72b528529234361e
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-09-15 18:52:39 +0330

    debug in design recover component

--- client/src/app/components/recovery/recovery.component.ts
+++ client/src/app/components/recovery/recovery.component.ts
@@ -3,18 +3,14 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recovery',
+  standalone: true,
   imports: [],
   templateUrl: './recovery.component.html',
-  styleUrl: './recovery.component.scss'
+  styleUrls: ['./recovery.component.scss']
 })
 export class RecoveryComponent {
   constructor(private router: Router) { }
 
-  goHome(): void {
-    this.router.navigateByUrl('/');
-  }
-
-  contact(): void {
-    this.router.navigateByUrl('/');
-  }
+  goHome(): void { this.router.navigateByUrl('/'); }
+  contact(): void { this.router.navigateByUrl('/'); }
 }
