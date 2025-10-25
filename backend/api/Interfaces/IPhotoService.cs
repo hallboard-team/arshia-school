13d2c591943941bf0130f48efadd4dee28816a30
Reza Taba <mrtabaa@gmail.com>
2025-10-25 15:46:08 +0400

    Upgraded and renamed Image_Processing

--- backend/api/Interfaces/IPhotoService.cs
+++ backend/api/Interfaces/IPhotoService.cs
@@ -2,6 +2,7 @@ namespace api.Interfaces;
 
 public interface IPhotoService
 {
-    public Task<string[]?> AddPhotoToDiskAsync(IFormFile file, string productId);
-    public Task<bool> DeletePhotoFromDisk(Photo photo);
+  public Task<string[]?> AddPhotoToDiskAsync(IFormFile file, ObjectId userId);
+
+  public Task<bool> DeletePhotoFromDisk(Photo photo);
 }
