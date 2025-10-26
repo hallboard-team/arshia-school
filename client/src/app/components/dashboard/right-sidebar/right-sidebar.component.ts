c6338fc38399fb76f926fc48fbff842128577ace
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-10-26 10:01:58 +0330

    Design UI dashboard updated (#188)

--- client/src/app/components/dashboard/right-sidebar/right-sidebar.component.ts
+++ client/src/app/components/dashboard/right-sidebar/right-sidebar.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit, Signal } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { AccountService } from '../../../services/account.service';
 import { LoggedInUser } from '../../../models/logged-in-user.model';
+import { UserProfile } from '../../../models/user-profile.model';
+import { MemberService } from '../../../services/member.service';
 
 @Component({
   selector: 'app-right-sidebar',
@@ -13,9 +15,19 @@ export class RightSidebarComponent implements OnInit {
   private _route = inject(ActivatedRoute);
   private accountService = inject(AccountService);
 
+  public memberService = inject(MemberService);
+
   loggedInUserSig: Signal<LoggedInUser | null> | undefined;
+
+  profile: UserProfile | null = null;
+  error: string | null = null;
 
   ngOnInit(): void {
     this.loggedInUserSig = this.accountService.loggedInUserSig;
+  }
+
+  logout(): void {
+    this.accountService.logout();
+    // this.closeProfile();
   }
 }
