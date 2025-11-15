038602358720c481b5b324ab7c8a08c2b2289583
realpj10101 <parsajfr1385oo@gmail.com>
2025-11-16 00:19:05 +0330

    Created Add member photo to disk and delete member photo from disk methods

--- backend/api/Interfaces/IPhotoService.cs
+++ backend/api/Interfaces/IPhotoService.cs
@@ -2,7 +2,9 @@ namespace api.Interfaces;
 
 public interface IPhotoService
 {
+  public Task<string[]?> AddMemberPhotoToDiskAsync(IFormFile file, MemberPhoto? photo, ObjectId userId);
   public Task<string[]?> AddPhotoToDiskAsync(IFormFile file, ObjectId userId);
 
   public Task<bool> DeletePhotoFromDisk(Photo photo);
+  public Task<bool> DeleteMemberPhotoFromDiskAsync(MemberPhoto photo);
 }
