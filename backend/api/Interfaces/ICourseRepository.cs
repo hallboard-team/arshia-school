98bdc52a41db1dcfa7a6af8f30fd1dd2dadc5176
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-13 02:46:00 +0330

    Updated course controller, interface and repo with new course entity structure

--- backend/api/Interfaces/ICourseRepository.cs
+++ backend/api/Interfaces/ICourseRepository.cs
@@ -1,14 +1,13 @@
+using api.DTOs.Helpers;
+
 namespace api.Interfaces;
 
 public interface ICourseRepository
 {
-    public Task<ShowCourseDto> AddCourseAsync(AddCourseDto managerInput, CancellationToken cancellationToken);
+    public Task<OperationResult<ShowCourseDto>> AddCourseAsync(CreateCourseDto managerInput, CancellationToken cancellationToken);
     public Task<PagedList<Course>> GetAllAsync(PaginationParams paginationParams, CancellationToken cancellationToken);
-    public Task<List<string>> GetProfessorUserNamesByIdsAsync(List<ObjectId> professorIds, CancellationToken cancellationToken);
-    public Task<List<string>> GetProfessorNamesByIdsAsync(List<ObjectId> professorIds, CancellationToken cancellationToken);
-    public Task<bool> UpdateCourseAsync(UpdateCourseDto updateCourseDto, string targetCourseTitle, CancellationToken cancellationToken);
-    public Task<bool> AddProfessorToCourseAsync(string targetCourseTitle, string professorUserName, CancellationToken cancellationToken);
-    public Task<bool> RemoveProfessorFromCourseAsync(string targetCourseTitle, string professorName, CancellationToken cancellationToken);
-    public Task<ShowCourseDto?> GetCourseByTitleAsync(string courseTitle, CancellationToken cancellationToken);
-    public Task<List<ShowClassAndTitleDto>> GetClassesAndTitles(CancellationToken cancellationToken);
+    public Task<OperationResult<ShowCourseDto>> UpdateCourseAsync(UpdateCourseDto updateCourseDto, string targetCourseTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<ShowCourseDto>> GetCourseByTitleAsync(string courseTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<ShowCourseDto>> GetCourseByIdAsync(ObjectId courseId, CancellationToken cancellationToken);
+    public Task<OperationResult> DeleteCourseAsync(string courseName, CancellationToken cancellationToken);
 }
