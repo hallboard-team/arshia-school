925196b11b72b3310b78337b589ec048986ee56c
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-14 01:00:26 +0330

    Fixed property naming

--- backend/api/DTOs/_Mappers.cs
+++ backend/api/DTOs/_Mappers.cs
@@ -109,7 +109,7 @@ public static class Mappers
     {
         return new UserWithRoleDto(
             UserName: appUser.NormalizedUserName!,
-            Roles: appUser.appRoles
+            Roles: appUser.AppRoles
         );
     }
 
--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -15,6 +15,6 @@ public class AppUser : MongoIdentityUser<ObjectId>
     [BsonSerializer(typeof(SafeGenderSerializer))]
     public GenderType Gender { get; init; } = GenderType.Unknown;
     public List<EnrolledClass> EnrolledClasses { get; init; } = [];
-    public List<string> appRoles { get; init; } = [];
+    public List<string> AppRoles { get; init; } = [];
     public MemberPhoto? Photo { get; set; } = null;
 }
