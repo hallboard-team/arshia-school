b6214af2979dfdd081b3a16fbb0c9bea9908117c
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-21 20:12:04 +0330

    Changed member interface return types to op result

--- backend/api/Interfaces/IMemberRepository.cs
+++ backend/api/Interfaces/IMemberRepository.cs
@@ -4,9 +4,9 @@ namespace api.Interfaces;
 
 public interface IMemberRepository
 {
-    public Task<ProfileDto?> GetProfileAsync(string hashedUserId, CancellationToken cancellationToken);
-    public Task<PagedList<Attendance>> GetAllAttendenceAsync(AttendenceParams attendenceParams, ObjectId? userId, string targetClassTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<ProfileDto>> GetProfileAsync(string hashedUserId, CancellationToken cancellationToken);
+    public Task<OperationResult<PagedList<Attendance>>> GetAllAttendenceAsync(AttendenceParams attendenceParams, ObjectId? userId, string targetClassTitle, CancellationToken cancellationToken);
     public Task<OperationResult<TargetMemberDto>> UpdateMemberAsync(MemberUpdateDto memberUpdateDto, ObjectId userId, CancellationToken cancellationToken);
-    public Task<List<Class>> GetClassesAsync(string hashedUserId, CancellationToken cancellationToken);
-    public Task<EnrolledClass?> GetEnrolledCourseAsync(string hashedUserId, string courseTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<List<Class>>> GetClassesAsync(string hashedUserId, CancellationToken cancellationToken);
+    public Task<OperationResult<EnrolledClass>> GetEnrolledCourseAsync(string hashedUserId, string courseTitle, CancellationToken cancellationToken);
 }
