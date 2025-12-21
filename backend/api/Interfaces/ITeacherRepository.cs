0c0e846592f35ecb7cd603e192bfcb4980e2a6ae
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-21 20:38:16 +0330

    Changed check is absent interface return type to op result

--- backend/api/Interfaces/ITeacherRepository.cs
+++ backend/api/Interfaces/ITeacherRepository.cs
@@ -8,5 +8,5 @@ public interface ITeacherRepository
     Task<OperationResult<ShowStudentStatusDto>> AddAsync(AddStudentStatusDto teacherInput, string targetCourseTitle, CancellationToken cancellationToken);
     Task<OperationResult> DeleteAsync(ObjectId userId, string targetUserName, string targetCourseTitle, DateOnly currentDate, CancellationToken cancellationToken);
     Task<OperationResult<PagedList<AppUser>>> GetAllAsync(PaginationParams paginationParams, string targetTitle, string hashedUserId, CancellationToken cancellationToken);
-    Task<Dictionary<ObjectId, bool>> CheckIsAbsentAsync(List<ObjectId> studentIds, ObjectId courseId, CancellationToken cancellationToken);
+    Task<OperationResult<Dictionary<ObjectId, bool>>> CheckIsAbsentAsync(List<ObjectId> studentIds, ObjectId courseId, CancellationToken cancellationToken);
 }
