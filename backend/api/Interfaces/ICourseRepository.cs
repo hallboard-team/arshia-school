d2a1580a904c70f0d4ffce3db126413f2d5259ee
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-21 13:44:30 +0330

    Changed course interface return type to op result

--- backend/api/Interfaces/ICourseRepository.cs
+++ backend/api/Interfaces/ICourseRepository.cs
@@ -5,8 +5,8 @@ namespace api.Interfaces;
 public interface ICourseRepository
 {
     public Task<OperationResult<ShowCourseDto>> AddCourseAsync(CreateCourseDto managerInput, CancellationToken cancellationToken);
-    public Task<PagedList<Course>> GetAllAsync(PaginationParams paginationParams, CancellationToken cancellationToken);
-    public Task<OperationResult<ShowCourseDto?>> UpdateCourseAsync(UpdateCourseDto updateCourseDto, string targetCourseTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<PagedList<Course>>> GetAllAsync(PaginationParams paginationParams, CancellationToken cancellationToken);
+    public Task<OperationResult<ShowCourseDto>> UpdateCourseAsync(UpdateCourseDto updateCourseDto, string targetCourseTitle, CancellationToken cancellationToken);
     public Task<OperationResult<ShowCourseDto>> GetCourseByTitleAsync(string courseTitle, CancellationToken cancellationToken);
     public Task<OperationResult<ShowCourseDto>> GetCourseByIdAsync(ObjectId courseId, CancellationToken cancellationToken);
     public Task<OperationResult> DeleteCourseAsync(string courseName, CancellationToken cancellationToken);
