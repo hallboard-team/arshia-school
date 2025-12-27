b62e6b8f02bd6947cd29be147877052104f04200
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-27 23:40:22 +0330

    refactor(api): unify  member interface using operation result
    Refactor member interfaces for consistency

--- backend/api/Interfaces/IMemberRepository.cs
+++ backend/api/Interfaces/IMemberRepository.cs
@@ -7,6 +7,6 @@ public interface IMemberRepository
     public Task<OperationResult<ProfileDto>> GetProfileAsync(string hashedUserId, CancellationToken cancellationToken);
     public Task<OperationResult<PagedList<Attendance>>> GetAllAttendenceAsync(AttendenceParams attendenceParams, ObjectId? userId, string targetClassTitle, CancellationToken cancellationToken);
     public Task<OperationResult<TargetMemberDto>> UpdateMemberAsync(MemberUpdateDto memberUpdateDto, ObjectId userId, CancellationToken cancellationToken);
-    public Task<List<ClassRoom>> GetClassesAsync(string hashedUserId, CancellationToken cancellationToken);
-    public Task<EnrolledClassRoom?> GetEnrolledCourseAsync(string hashedUserId, string courseTitle, CancellationToken cancellationToken);
+    public Task<OperationResult<List<ClassRoom>>> GetClassesAsync(string hashedUserId, CancellationToken cancellationToken);
+    public Task<OperationResult<EnrolledClassRoom>> GetEnrolledCourseAsync(string hashedUserId, string courseTitle, CancellationToken cancellationToken);
 }
