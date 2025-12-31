0183f001fb1b0389563e82c157b7464910cceb7b
Reza Taba <mrtabaa@gmail.com>
2025-12-31 17:10:57 +0000

    feat(fullstack): connect client, .net and mongodb

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // production: false,
-    apiUrl: 'http://localhost:5000/api/',
-    apiPhotoUrl: 'http://localhost:5000/'
+    apiUrl: 'http://localhost:5312/api/',
+    apiPhotoUrl: 'http://localhost:5312/'
 };
